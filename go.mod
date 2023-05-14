fb3f312cf580e737c9bda14795a1e90d32b58099
Madhur <ahuja.madhur@gmail.com>
2023-05-14 08:21:59 +0530

    feat: add dependency

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/madhur/puppy
 
 go 1.20
+
+require github.com/madhur/dog v0.0.0-20230514024841-da26d1d76e09 // indirect
--- puppy.go
+++ puppy.go
@@ -1,9 +1,21 @@
 package puppy
 
+import (
+	"github.com/madhur/dog"
+)
+
 func Bark() string {
 	return "I am barking"
 }
 
 func Barks() string {
 	return "Woof! woof! woof!"
+}
+
+func BigBark() string {
+	return dog.WhenGrownUp(Bark())
+}
+
+func BigBarks() string {
+	return dog.WhenGrownUp(Barks())
 }
